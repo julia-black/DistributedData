317e223ed8792385b3e126af7fd3a8aa60ce0e4d
Julia Shevchenko <j.shevchenko@kingbird.ru>
2020-04-08 22:54:38 +0400

    Done Lab 2

--- Lab_2/src/com/ssu/juliablack/Main.java
+++ Lab_2/src/com/ssu/juliablack/Main.java
@@ -1,44 +1,69 @@
 package com.ssu.juliablack;
 
 import javax.imageio.ImageIO;
-import java.awt.image.*;
-import java.io.ByteArrayOutputStream;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Base64;
+import java.util.concurrent.CountDownLatch;
 
 class Main {
 
-    public static void main(String[] args) throws IOException {
-        String directory = "F:\\";
+    public static final String DIRECTORY = "F:\\";
 
-        BufferedImage image = ImageIO.read(new File(directory, "image.png"));
-        byte[] byteImage = imageToByteArray(image);
+    public static CountDownLatch countDownLatch;
 
-        BufferedImage watermark = ImageIO.read(new File(directory, "watermark.png"));
-        byte[] byteWatermark = imageToByteArray(watermark);
+    public static BufferedImage image;
 
-        for (int i = 0; i < byteWatermark.length; i++) {
-            byteImage[i] = byteWatermark[i];
+    public static BufferedImage watermark;
+
+    public static void main(String[] args) {
+        countDownLatch = new CountDownLatch(2);
+
+        new Thread(Main::readImage).start();
+        new Thread(Main::readWatermark).start();
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        int width = 1500;
-        int height = 1500;
+        BufferedImage bufferedImage = drawWatermark(image, watermark);
 
-        DataBuffer buffer = new DataBufferByte(byteImage, byteImage.length);
-        WritableRaster raster = Raster.createInterleavedRaster(buffer, width, height, 3 * width, 3, new int[]{0, 1, 2}, null);
-        image.setData(raster);
-        BufferedImage resultImage = new BufferedImage(image.getColorModel(), image.getRaster(), true, null);
-        ImageIO.write(resultImage, "png", new File(directory, "resultImage.png"));
+        try {
+            ImageIO.write(bufferedImage, "png", new File(DIRECTORY, "resultImage.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static byte[] imageToByteArray(BufferedImage image) throws IOException {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream(1000);
-        ImageIO.write(image, "png", baos);
-        baos.flush();
-        String base64String = Base64.getEncoder().encodeToString(baos.toByteArray());
-        baos.close();
+    public static void readImage() {
+        try {
+            Main.image = ImageIO.read(new File(DIRECTORY, "image.png"));
+            countDownLatch.countDown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        return Base64.getDecoder().decode(base64String);
+    public static void readWatermark() {
+        try {
+            Main.watermark = ImageIO.read(new File(DIRECTORY, "watermark.png"));
+            countDownLatch.countDown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static BufferedImage drawWatermark(BufferedImage image, BufferedImage watermark) {
+        Graphics2D graphics = image.createGraphics();
+        graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f));
+        graphics.drawImage(
+                watermark,
+                image.getWidth() / 2 - watermark.getWidth() / 2,
+                image.getHeight() / 2 - watermark.getHeight() / 2,
+                null);
+        return image;
     }
 }
